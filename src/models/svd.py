ec709ebbbdb9fb01b5865d9fa67e2a051bab1c24
Johnny Chiu <johnnychiu@Johnnyde-MacBook-Pro.local>
2018-03-03 00:36:26 -0600

    update config

--- src/models/svd.py
+++ src/models/svd.py
@@ -57,9 +57,16 @@ class mySVD():
         # conn = pymysql.connect(os.environ.get('HOST'), user=os.environ.get('USER'), port=int(os.environ.get('PORT')),
         #                        passwd=os.environ.get('PASSWORD'), db=os.environ.get('DBNAME'))
 
-        # try tutorial version
-        conn = pymysql.connect(os.environ['HOST'], user=os.environ['USER'], port=int(os.environ['PORT']),
-                               passwd=os.environ['PASSWORD'], db=os.environ['DBNAME'])
+        # # try tutorial version
+        # conn = pymysql.connect(os.environ['HOST'], user=os.environ['USER'], port=int(os.environ['PORT']),
+        #                        passwd=os.environ['PASSWORD'], db=os.environ['DBNAME'])
+
+        # try just using real host and everything
+        conn = pymysql.connect("aag31r5zpl942v.c7rrmd1b0hyo.us-west-2.rds.amazonaws.com",
+                               user="ebusermarch02",
+                               port=3306,
+                               passwd="12345678",
+                               db="ebdb")
 
         song_df = pd.read_sql('SELECT * FROM Song;', con=conn)
 
